b0d00169ff40486bfc4d2f71bf6f3e42e6ef427a
Messias Ferreira Pichau <125512149+messiaspichaujr@users.noreply.github.com>
2025-09-04 22:53:53 -0300

    Classe de segurança dos endpoints feita

--- backend/src/main/java/pt/cursinhoinsercao/portalaluno/Main.java
+++ backend/src/main/java/pt/cursinhoinsercao/portalaluno/Main.java
@@ -6,8 +6,8 @@ import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.servlet.ServletContainer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
 
+    public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
@@ -16,16 +16,14 @@ public class Main {
 
         ServletHolder servletHolder = context.addServlet(ServletContainer.class, "/api/*");
         servletHolder.setInitOrder(0);
-
         servletHolder.setInitParameter(
                 "jersey.config.server.provider.packages",
-                "pt.cursinhoinsercao.portalaluno.resource"
+                "pt.cursinhoinsercao.portalaluno"
         );
 
         try {
             server.start();
             System.out.println("Servidor iniciado em http://localhost:8080");
-
             server.join();
         } finally {
             server.destroy();
--- backend/src/main/java/pt/cursinhoinsercao/portalaluno/filter/AuthenticationFilter.java
+++ backend/src/main/java/pt/cursinhoinsercao/portalaluno/filter/AuthenticationFilter.java
@@ -1,4 +1,57 @@
 package pt.cursinhoinsercao.portalaluno.filter;
 
-public class AuthenticationFilter {
+import io.jsonwebtoken.Claims;
+import pt.cursinhoinsercao.portalaluno.seguranca.Seguranca;
+import pt.cursinhoinsercao.portalaluno.service.TokenService;
+
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Seguranca
+@Provider
+@Priority(Priorities.AUTHENTICATION)
+public class AuthenticationFilter implements ContainerRequestFilter {
+
+    private TokenService tokenService = new TokenService();
+    private static final int TIPO_ADMIN = 1; //professor com id 1 é o Admin.
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+
+        String authorizationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            abortarComNaoAutorizado(requestContext, "Cabeçalho de autorização ausente ou mal formatado.");
+            return;
+        }
+
+        String token = authorizationHeader.substring("Bearer".length()).trim();
+
+        try {
+            Claims claims = tokenService.validarToken(token);
+
+            int tipoUsuario = claims.get("tipo", Integer.class);
+            if (tipoUsuario != TIPO_ADMIN) {
+                requestContext.abortWith(
+                        Response.status(Response.Status.FORBIDDEN).entity("Acesso negado. Permissões insuficientes.").build());
+                return;
+            }
+
+
+        } catch (Exception e) {
+            // se o token for inválido ele lança essa mensagem
+            abortarComNaoAutorizado(requestContext, "Token inválido ou expirado.");
+        }
+    }
+
+    private void abortarComNaoAutorizado(ContainerRequestContext requestContext, String mensagem) {
+        requestContext.abortWith(
+                Response.status(Response.Status.UNAUTHORIZED).entity(mensagem).build());
+    }
 }
