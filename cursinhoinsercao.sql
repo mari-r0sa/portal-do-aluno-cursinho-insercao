29fda6a5045f2383dfe48af98251db266464b343
Messias Ferreira Pichau <125512149+messiaspichaujr@users.noreply.github.com>
2025-09-12 01:22:15 -0300

    Acrescentei o sql do banner

--- cursinhoinsercao.sql
+++ cursinhoinsercao.sql
@@ -35,16 +35,14 @@ CREATE TABLE `avaliacoes` (
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `banner`
+-- Estrutura da tabela `banner` (Versão com histórico)
 --
-
 CREATE TABLE `banner` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `imagem` varchar(300) NOT NULL,
   `ativo` tinyint(1) NOT NULL DEFAULT 0,
-  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -157,6 +155,8 @@ CREATE TABLE `secoes` (
   `imagem` varchar(70) DEFAULT NULL,
   `texto` varchar(500) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `secoes` ADD `titulo` VARCHAR(100) NOT NULL AFTER `id`;
 
 -- --------------------------------------------------------
 
